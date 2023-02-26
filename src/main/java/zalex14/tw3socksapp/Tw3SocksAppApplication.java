b3e9c1325885d9e3a48e1023f6b908411ca84120
zalex14 <9824018@gmail.com>
2023-02-26 10:18:57 +0300

    Курсовая работа 3 Склад носков

---
+++ src/main/java/zalex14/tw3socksapp/Tw3SocksAppApplication.java
@@ -0,0 +1,13 @@
+package zalex14.tw3socksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tw3SocksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tw3SocksAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zalex14/tw3socksapp/Tw3SocksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package zalex14.tw3socksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tw3SocksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
